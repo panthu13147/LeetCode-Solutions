4b06c1d61422be15f2f32760d1705d364bd50d97
panth  chetan shah <panthu13147@gmail.com>
2026-06-20 00:05:12 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.29%) - LeetHub

---
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water-06-20-2026-00-05-11.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int result=0;
+        int left_max=0,right_max=0;
+        int left,right;
+            left=0;
+            right=height.size()-1;
+            while(left<right)
+            {
+                if(height[left]<=height[right])
+                {
+                    if(height[left]>left_max)
+                    {
+                        left_max=height[left];
+
+                    }
+                    else
+                    {
+                        result+=left_max-height[left];
+                    }
+                    left++;
+                }
+                else
+                {
+                     if(height[right]>right_max)
+                    {
+                        right_max=height[right];
+
+                    }
+                    else
+                    {
+                        result+=right_max-height[right];
+                    }
+                    right--;
+                }
+              
+                
+            }
+            return result;
+
+        
+
+
+
+        
+        
+    }
+};
