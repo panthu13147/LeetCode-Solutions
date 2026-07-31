c54a3d177d3f4019f0a78d7ad50bfe4cd5b3748c
panth  chetan shah <panthu13147@gmail.com>
2026-07-31 21:20:21 +0530

    Time: 0 ms (100%), Space: 8.9 MB (69.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        bool word=true;
+        int length=0;
+        while(i>=0 && s[i]==' ')
+        {
+            i--;
+        }
+        
+        while(i>=0 && s[i]!=' ')
+        {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
